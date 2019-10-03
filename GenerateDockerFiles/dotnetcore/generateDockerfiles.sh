847bf3393801c6023b93c398c4e922318c85455e
Sanchit Mehta <sanchit.mehta602@gmail.com>
2019-10-02 18:21:04 -0700

    Some more fixes

--- GenerateDockerFiles/dotnetcore/generateDockerfiles.sh
+++ GenerateDockerFiles/dotnetcore/generateDockerfiles.sh
@@ -10,12 +10,13 @@ set -e
 
 declare -r DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
-declare -r ORYX_BASE_IMAGE_NAME="$1"
-declare -r ORYX_VERSION_STREAM_FEED="$2"  #
-declare -r APP_SVC_BRANCH_PREFIX="$3"     # appsvc, appsvctest
-declare -r APPSVC_DOTNETCORE_REPO="$4"    # https://github.com/Azure-App-Service/dotnetcore.git
+declare -r SYSTEM_ARTIFACTS_DIR="$1"
+declare -r ORYX_BASE_IMAGE_NAME="$2"
+declare -r ORYX_VERSION_STREAM_FEED="$3"  #
+declare -r APP_SVC_BRANCH_PREFIX="$4"     # appsvc, appsvctest
+declare -r APPSVC_DOTNETCORE_REPO="$5"    # https://github.com/Azure-App-Service/dotnetcore.git
 
-declare -r APP_SVC_REPO_DIR="$DIR/GitRepo"
+declare -r APP_SVC_REPO_DIR="$SYSTEM_ARTIFACTS_DIR/GitRepo"
 declare -r APP_SVC_REPO_BRANCH="dev"
 declare -r STACK_NAME="dotnetcore"
 
@@ -23,33 +24,33 @@ declare -r STACK_NAME="dotnetcore"
 function generateDockerFiles()
 {
 	local versionsFile="$1"
-        local dockerTemplateDir="$2"
-        local baseImageName="$3"
-        local baseImageVersionStreamFeed="$4"
+    local dockerTemplateDir="$2"
+    local baseImageName="$3"
+    local baseImageVersionStreamFeed="$4"
 
 	# Example line:
 	# 1.0 -> uses Oryx Base Image mcr.microsoft.com/oryx/dotnetcore:1.0-$ORYX_VERSION_STREAM_FEED
 	while IFS= read -r STACK_VERSION || [[ -n $STACK_VERSION ]]
 	do
         FINAL_IMAGE_NAME="$(echo -e "${APP_SVC_BRANCH_PREFIX}/${STACK_NAME}:${STACK_VERSION}_${baseImageVersionStreamFeed}" | sed -e 's/^[[:space:]]*//')"
-        
+
         # Oryx Image
         BASE_IMAGE_NAME="$baseImageName:$baseImageVersionStreamFeed"
-        VERSION_DIRECTORY="${APP_SVC_REPO_DIR}/${STACK_VERSION}"
-        TARGET_DOCKERFILE="${VERSION_DIRECTORY}/Dockerfile"
-        
-        echo "Generating Dockerfile for image '$FINAL_IMAGE_NAME' in directory '$VERSION_DIRECTORY'..."
+        CURR_VERSION_DIRECTORY="${APP_SVC_REPO_DIR}/${STACK_VERSION}"
+        TARGET_DOCKERFILE="${CURR_VERSION_DIRECTORY}/Dockerfile"
+
+        echo "Generating Dockerfile for image '$FINAL_IMAGE_NAME' in directory '$CURR_VERSION_DIRECTORY'..."
 
         # Remove Existing Version directory, eg: GitRepo/1.0 to replace with realized files
-        rm -rf "$VERSION_DIRECTORY"
-        mkdir -p "$VERSION_DIRECTORY"
-        cp -R $dockerTemplateDir/* "$VERSION_DIRECTORY"
+        rm -rf "$CURR_VERSION_DIRECTORY"
+        mkdir -p "$CURR_VERSION_DIRECTORY"
+        cp -R $dockerTemplateDir/* "$CURR_VERSION_DIRECTORY"
 
         # Replace placeholders
-		 sed -i "s/BASE_IMAGE_NAME_PLACEHOLDER/$BASE_IMAGE_NAME/g" "$TARGET_DOCKERFILE"
+		sed -i "s/BASE_IMAGE_NAME_PLACEHOLDER/$BASE_IMAGE_NAME/g" "$TARGET_DOCKERFILE"
 
 		# Copy Hosting Start App
-        cp "$DIR/SampleApps/$STACK_VERSION/bin.zip" "${VERSION_DIRECTORY}/"
+        cp "$DIR/SampleApps/$STACK_VERSION/bin.zip" "${CURR_VERSION_DIRECTORY}/"
 
 	done < "$versionsFile"
 }
@@ -58,11 +59,11 @@ function pullAppSvcRepo()
 {
     echo "Cloning App Service DOTNETCORE Repository in $APP_SVC_REPO_DIR"
     git clone $APPSVC_DOTNETCORE_REPO $APP_SVC_REPO_DIR
-    cho "Cloning App Service DOTNETCORE Repository in $APP_SVC_REPO_DIR"
+    echo "Cloning App Service DOTNETCORE Repository in $APP_SVC_REPO_DIR"
     cd $APP_SVC_REPO_DIR
     echo "Checking out branch $APP_SVC_REPO_BRANCH"
     git checkout $APP_SVC_REPO_BRANCH
-    chmod -R 777 *
+    chmod -R 777 $APP_SVC_REPO_DIR
 }
 
 pullAppSvcRepo
