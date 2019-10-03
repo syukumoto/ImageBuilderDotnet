035b791cecff990f5765dd87d0421a4b540c0e78
Sanchit Mehta <sanmeht@microsoft.com>
2019-10-03 15:36:09 +0000

    Added Node JS Support

--- GenerateDockerFiles/node/README.md
+++ GenerateDockerFiles/node/README.md
@@ -1,24 +1,12 @@
-# Node images
+# Azure App Service Linux Node.js image templates
 
-The Dockerfiles for our Node images are either produced by a template, which derives from the official image, or
-created manually from the `buildpack-deps` base.
+These are the templates used to the Node.js images
 
-# Images derived from `buildpack-deps`
+Realized Dockerfiles are at: https://github.com/azure-app-service/node
 
-The reason we do rebuild some old images instead of relying on the official ones is security: since
-the official images for older versions were made using the version of `buildpack-deps` available at the time, which
-contained vulnerabilities, we rebuild them using the current version of that base image. The portion of our
-Dockerfiles that install Node (which we always have in its own stage called `node-original-image`) are identical to the
-ones used in the official Node image for each particular version.
+Base Image: https://github.com/microsoft/Oryx/tree/master/images/runtime/node
 
-# Generate images from template
-
-Since our node images are identical except for the base image, which determines the node version,
-our final node images are generated from a template file, `./Dockerfile.template`, which contains the placeholder
-for the base image name. 
-
-To generate the dockerfiles for all the images, run `./generateDockerfiles.sh`.
-
-## Adding new versions of Node
-
-To add a new Node version, add a new directory with the version name, and a base Dockerfile named `./Dockerfile.base`, that should contain the node binaries. The script `./generateDockerfiles.sh` will then produce the Dockerfile for the final image using the `./Dockerfile.base` image as its base.
+# Version Mapping
+|Node.js Version | Template Name|
+|----------------|--------------|
+| all            | debian-9     |
--- GenerateDockerFiles/node/generateDockerfiles.sh
+++ GenerateDockerFiles/node/generateDockerfiles.sh
@@ -2,25 +2,69 @@
 # --------------------------------------------------------------------------------------------
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT license.
+# This script generates Dockerfiles for ASP .NETCore Runtime Images for Azure App Service on Linux.
 # --------------------------------------------------------------------------------------------
 
 set -e
 
+# Current Working Dir
 declare -r DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
-declare -r DOCKERFILE_TEMPLATE="$DIR/Dockerfile.template"
-declare -r IMAGE_NAME_PLACEHOLDER="%NODE_BASE_IMAGE%"
-declare -r RUNTIME_BASE_IMAGE_NAME_PLACEHOLDER="%RUNTIME_BASE_IMAGE_NAME%"
-declare -r BASE_BUILD_NUMBER="20190826.1"
+# Directory for Generated Docker Files
+declare -r SYSTEM_ARTIFACTS_DIR="$1"
+declare -r BASE_IMAGE_REPO_NAME="$2"                 # mcr.microsoft.com/oryx/dotnetcore
+declare -r BASE_IMAGE_VERSION_STREAM_FEED="$3"  # Base Image Version; Oryx Version : 20190819.2
+declare -r APP_SVC_BRANCH_PREFIX="$4"           # appsvc, appsvctest
+declare -r APPSVC_DOTNETCORE_REPO="$5"          # https://github.com/Azure-App-Service/dotnetcore.git
+declare -r APP_SVC_REPO_BRANCH="$6"             # dev
+declare -r STACK_VERSIONS_FILE_PATH="$7"
+declare -r STACK_NAME="node"
+declare -r APP_SVC_REPO_DIR="$SYSTEM_ARTIFACTS_DIR/$STACK_NAME/GitRepo"
 
-cd $DIR
-for VERSION_DIRECTORY in $(find . -type d -iname '[0-9]*' -printf '%f\n')
-do
-	echo "Generating Dockerfile for image $VERSION_DIRECTORY..."
 
-	TARGET_DOCKERFILE="$DIR/$VERSION_DIRECTORY/Dockerfile"
-	cp "$DOCKERFILE_TEMPLATE" "$TARGET_DOCKERFILE"
 
-	# Replace placeholders
-	RUNTIME_BASE_IMAGE_NAME="mcr.microsoft.com/oryx/node-base:$VERSION_DIRECTORY-$BASE_BUILD_NUMBER"
-	sed -i "s|$RUNTIME_BASE_IMAGE_NAME_PLACEHOLDER|$RUNTIME_BASE_IMAGE_NAME|g" "$TARGET_DOCKERFILE"
-done
+function generateDockerFiles()
+{
+    local dockerTemplateDir="$1"
+
+	# Example line:
+	# 1.0 -> uses Oryx Base Image mcr.microsoft.com/oryx/dotnetcore:1.0-$BASE_IMAGE_VERSION_STREAM_FEED
+	while IFS= read -r STACK_VERSION || [[ -n $STACK_VERSION ]]
+	do
+        FINAL_IMAGE_NAME="$(echo -e "${APP_SVC_BRANCH_PREFIX}/${STACK_NAME}:${STACK_VERSION}_${BASE_IMAGE_VERSION_STREAM_FEED}" | sed -e 's/^[[:space:]]*//')"
+
+        # Base Image
+        BASE_IMAGE_NAME="$BASE_IMAGE_REPO_NAME:$BASE_IMAGE_VERSION_STREAM_FEED"
+        CURR_VERSION_DIRECTORY="${APP_SVC_REPO_DIR}/${STACK_VERSION}"
+        TARGET_DOCKERFILE="${CURR_VERSION_DIRECTORY}/Dockerfile"
+
+        echo "Generating Dockerfile for image '$FINAL_IMAGE_NAME' in directory '$CURR_VERSION_DIRECTORY'..."
+
+        # Remove Existing Version directory, eg: GitRepo/1.0 to replace with realized files
+        rm -rf "$CURR_VERSION_DIRECTORY"
+        mkdir -p "$CURR_VERSION_DIRECTORY"
+        cp -R $dockerTemplateDir/* "$CURR_VERSION_DIRECTORY"
+
+        # Replace placeholders, changing sed delimeter since '/' is used in path
+		sed -i "s|BASE_IMAGE_NAME_PLACEHOLDER|$BASE_IMAGE_NAME|g" "$TARGET_DOCKERFILE"
+
+		# Copy Hosting Start App
+        cp "$DIR/SampleApps/$STACK_VERSION/bin.zip" "${CURR_VERSION_DIRECTORY}/"
+        
+        echo "Done."
+
+	done < "$STACK_VERSIONS_FILE_PATH"
+}
+
+function pullAppSvcRepo()
+{
+    echo "Cloning App Service DOTNETCORE Repository in $APP_SVC_REPO_DIR"
+    git clone $APPSVC_DOTNETCORE_REPO $APP_SVC_REPO_DIR
+    echo "Cloning App Service DOTNETCORE Repository in $APP_SVC_REPO_DIR"
+    cd $APP_SVC_REPO_DIR
+    echo "Checking out branch $APP_SVC_REPO_BRANCH"
+    git checkout $APP_SVC_REPO_BRANCH
+    chmod -R 777 $APP_SVC_REPO_DIR
+}
+
+pullAppSvcRepo
+generateDockerFiles "$DIR/debian-9"
