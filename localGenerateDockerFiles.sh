ce15a7e856f72b81d39c9aaba5ec3e156b5aeaa5
kumaramit-msft <74639899+kumaramit-msft@users.noreply.github.com>
2021-10-06 06:40:13 +0530

    Update oryx to 20211005.1 (#137)

--- localGenerateDockerFiles.sh
+++ localGenerateDockerFiles.sh
@@ -3,7 +3,7 @@
 # values from ImageBuilder/GenerateDockerFiles/dockerFilesGenerateTask.yml
 artifactStagingDirectory="output/DockerFiles"
 baseImageName="mcr.microsoft.com/oryx"
-baseImageVersion="20210928.1" # change me as needed
+baseImageVersion="20211005.1" # change me as needed
 appSvcGitUrl="https://github.com/Azure-App-Service"
 configDir="Config"
 
