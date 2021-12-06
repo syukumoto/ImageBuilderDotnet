1c6bd9c69cec40570ed7442896e7d398469a868a
Vijay Saayi R <Vijay.Saayi@microsoft.com>
2021-12-06 12:39:50 +0530

    Wrapping the directory creation code block in gunicorn.conf.py in try..except

--- GenerateDockerFiles/python/template-3.9/gunicorn.conf.py
+++ GenerateDockerFiles/python/template-3.9/gunicorn.conf.py
@@ -2,8 +2,12 @@ import constants
 import code_profiler_installer as cpi
 from pathlib import Path
 
-Path(constants.CODE_PROFILER_LOGS_DIR).mkdir(parents=True, exist_ok=True)
-pidfile = constants.PID_FILE_LOCATION
+try:
+    Path(constants.CODE_PROFILER_LOGS_DIR).mkdir(parents=True, exist_ok=True)
+    pidfile = constants.PID_FILE_LOCATION
+    
+except Exception as e:
+    print(f"Gunicorn was unable to set the pidfile path due to the exception : {e}")
 
 def post_worker_init(worker):
     try:
