06793df2a9497f85da0d2fab2fc3a9326df0e249
kumaramit-msft <74639899+kumaramit-msft@users.noreply.github.com>
2022-02-10 03:26:12 +0000

    Start Diagserver and dotnet-monitor using cron in .NET images (#175)
    Start Diagserver and dotnet-monitor using cron

---
+++ GenerateDockerFiles/dotnetcore/debian-9/run-dotnet-monitor.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+RESULT1=`pgrep dotnet-monitor`
+
+if [ "${RESULT1:-null}" = null ]; then
+    if [ -f "/dotnet_monitor_config.json" ]; then
+        if [ ! -f "$HOME/.config/dotnet-monitor/settings.json" ]; then
+            mkdir -p $HOME/.config/dotnet-monitor
+            cp /dotnet_monitor_config.json $HOME/.config/dotnet-monitor/settings.json
+        fi
+    fi
+    echo "dotnet-monitor not running, starting again..."
+    /opt/dotnetcore-tools/dotnet-monitor collect --urls "http://0.0.0.0:50051" --metrics true --metricUrls "http://0.0.0.0:50050" --no-auth > /dev/null 2>&1 &
+fi
