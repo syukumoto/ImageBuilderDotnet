3645da28e75609976a6a376a774abc131dd9ab0f
Sarath Chandra Bussa <bussasarath2@gmail.com>
2022-02-10 21:45:10 +0530

    changes to start up (#180)
    Co-authored-by: SARATH CHANDRA BUSSA <sbussa@microsoft.com>

--- GenerateDockerFiles/dotnetcore/debian-9/run-diag.sh
+++ GenerateDockerFiles/dotnetcore/debian-9/run-diag.sh
@@ -4,6 +4,6 @@ if [ -f "/diagServer/DiagServer" ]; then
     RESULT2=`pgrep DiagServer`
     if [ "${RESULT2:-null}" = null ]; then
         echo "DiagServer not running, starting again..."
-        /diagServer/DiagServer --urls "http://0.0.0.0:50055" > /dev/null 2>&1 &
+        (cd /diagServer && ./DiagServer --urls "http://0.0.0.0:50055" > /dev/null 2>&1) &
     fi
 fi
