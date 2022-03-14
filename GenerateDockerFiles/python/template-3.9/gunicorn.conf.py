c90697bca640d2e44185545bc8f20e5705654633
Vijay Saayi R <Vijay.Saayi@microsoft.com>
2022-03-14 14:37:08 +0530

    Rearranging the order of import

--- GenerateDockerFiles/python/template-3.9/gunicorn.conf.py
+++ GenerateDockerFiles/python/template-3.9/gunicorn.conf.py
@@ -1,5 +1,6 @@
-# appsvc_profiler is currenly installed from appsvc_code_profiler-1.0.0-py3-none-any.whl
 import appServiceAppLogs as asal
+
+# appsvc_profiler is currenly installed from appsvc_code_profiler-1.0.0-py3-none-any.whl
 from appsvc_profiler import CodeProfilerInstaller
 
 def post_worker_init(worker):
