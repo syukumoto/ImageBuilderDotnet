6bfa6839608058731bf1c45a841a3045dba0f5e3
Vijay Saayi R <Vijay.Saayi@microsoft.com>
2022-03-31 14:36:53 +0530

    Updating build script to honor stackversion if specified

--- BuildAndTagImages/build.sh
+++ BuildAndTagImages/build.sh
@@ -19,73 +19,108 @@ declare -r FILES_ROOT_PATH="$7"
 declare -r IMG_TAG="$8"
 declare -r WAWS_IMAGE_REPO_NAME="wawsimages.azurecr.io"
 declare -r ACR_BUILD_IMAGES_ARTIFACTS_FILE="$SYSTEM_ARTIFACTS_DIR/builtImages.txt"
+declare -r STACK_VERSION_TO_BUILD=$9
+
+function displayArtifactsDir()
+{
+    local artifactsDir=$1
+    echo "Listing artifacts dir :"
+    echo "--------------------------"
+    ls $artifactsDir
+    echo 
+}
+
+function displayStackDir()
+{
+    local stacksDir=$1
+    echo "Listing stacks dir :"
+    echo "--------------------------"
+    ls $stacksDir
+    echo 
+}
+
+function displayInformationRegardingImageToBeBuilt()
+{
+    local tag=$1
+    local dockerFilePath=$2
+    echo "Building test image with "
+    echo "--------------------------"
+    echo "Tag           : $BuildVerRepoTag "
+    echo "DockerFile    : $appSvcDockerfilePath"
+    echo
+}
 
 function buildDockerImage() 
 {
     if [ -f "$CONFIG_DIR/${STACK}VersionTemplateMap.txt" ]; then
         while IFS=, read -r STACK_VERSION BASE_IMAGE STACK_VERSION_TEMPLATE_DIR STACK_TAGS || [[ -n $STACK_VERSION ]] || [[ -n $BASE_IMAGE ]] || [[ -n $STACK_VERSION_TEMPLATE_DIR ]] || [[ -n $STACK_TAGS ]]
         do
-            echo "Stack Tag is ${STACK_TAGS}"
-            IFS='|' read -ra STACK_TAGS_ARR <<< "$STACK_TAGS"
-            for TAG in "${STACK_TAGS_ARR[@]}"
-            do
-                local STACK_MOD=$STACK
-                if [[ $STACK = "php-xdebug" ]]
-                then
-                    STACK_MOD="php"
-                fi
-                
-                if [[ $STACK = "wordpress" ]]
-                then
-                    STACK_MOD="wordpress-alpine-php"
-                fi
+            if [[ -z $STACK_VERSION_TO_BUILD || $STACK_VERSION_TO_BUILD == $STACK_VERSION ]];
+            then
+                echo "Stack Tag     : ${STACK_TAGS}"
+                echo "Stack Version : $STACK_VERSION"
+                echo 
 
-		local TAG_MOD="${TAG}_${PIPELINE_BUILD_NUMBER}"
-		if [[ $STACK = "wordpress" && ( $TAG = "latest"  ||  $TAG = "latest_7.4" ) ]]; then
-			TAG_MOD="${TAG}"
-		fi
+                IFS='|' read -ra STACK_TAGS_ARR <<< "$STACK_TAGS"
+                for TAG in "${STACK_TAGS_ARR[@]}"
+                do
+                    local STACK_MOD=$STACK
+                    if [[ $STACK = "php-xdebug" ]]
+                    then
+                        STACK_MOD="php"
+                    fi
 
-                # Build Image Tags are converted to lower case because docker doesn't accept upper case tags
-                local MCRRepoTagUpperCase="${WAWS_IMAGE_REPO_NAME}/public/appsvc/${STACK_MOD}:${TAG_MOD}"
-                local MCRRepoTag="${MCRRepoTagUpperCase,,}"
-                local appSvcDockerfilePath="${SYSTEM_ARTIFACTS_DIR}/${STACK}/GitRepo/${STACK_VERSION}/Dockerfile"
-                local BuildVerRepoTagUpperCase="${WAWS_IMAGE_REPO_NAME}/${STACK_MOD}:${TAG_MOD}"
-                local BuildVerRepoTag="${BuildVerRepoTagUpperCase,,}"
+                    if [[ $STACK = "wordpress" ]]
+                    then
+                        STACK_MOD="wordpress-alpine-php"
+                    fi
 
-                echo "Listing artifacts dir"
-                ls "${SYSTEM_ARTIFACTS_DIR}"
-                echo "Listing stacks dir"
-                ls "${SYSTEM_ARTIFACTS_DIR}/${STACK}/GitRepo/${STACK_VERSION}"
-                cd "${SYSTEM_ARTIFACTS_DIR}/${STACK}/GitRepo/${STACK_VERSION}"
+                    local TAG_MOD="${TAG}_${PIPELINE_BUILD_NUMBER}"
+                    if [[ $STACK = "wordpress" && ( $TAG = "latest"  ||  $TAG = "latest_7.4" ) ]]; then
+                        TAG_MOD="${TAG}"
+                    fi
 
-                echo "Building test image with tag '$BuildVerRepoTag' and file $appSvcDockerfilePath..."
+                    # Build Image Tags are converted to lower case because docker doesn't accept upper case tags
+                    local MCRRepoTagUpperCase="${WAWS_IMAGE_REPO_NAME}/public/appsvc/${STACK_MOD}:${TAG_MOD}"
+                    local MCRRepoTag="${MCRRepoTagUpperCase,,}"
+                    local appSvcDockerfilePath="${SYSTEM_ARTIFACTS_DIR}/${STACK}/GitRepo/${STACK_VERSION}/Dockerfile"
+                    local BuildVerRepoTagUpperCase="${WAWS_IMAGE_REPO_NAME}/${STACK_MOD}:${TAG_MOD}"
+                    local BuildVerRepoTag="${BuildVerRepoTagUpperCase,,}"
 
-                # php-xdebug depends of published images
-                if [ "$BUILD_REASON" != "PullRequest" ] || [ "$STACK" != "php-xdebug" ]; then
-                    echo docker build -t "$BuildVerRepoTag" -f "$appSvcDockerfilePath" .
-                    docker build -t "$BuildVerRepoTag" -f "$appSvcDockerfilePath" .
-                elif [ "$BUILD_REASON" != "PullRequest" ] && [ "$STACK" == "php-xdebug" ]; then
-                    # poll until php image is published
-                    BASE_TAG=`head -n 1 Dockerfile | sed 's/FROM //g'`
-                    PULL_OUTPUT=`docker pull $BASE_TAG`
-                    LOOP_COUNTER=0;
-                    while [[ $PULL_OUTPUT != *"up to date"* ]] && [ $LOOP_COUNTER -le 60 ]; do
-                        sleep 1m
-                        PULL_OUTPUT=$(docker pull $BASE_TAG)
-                        LOOP_COUNTER=$LOOP_COUNTER+1
-                    done;
-                    echo docker build -t "$BuildVerRepoTag" -f "$appSvcDockerfilePath" .
-                    docker build -t "$BuildVerRepoTag" -f "$appSvcDockerfilePath" .
-                fi
+                    displayArtifactsDir "${SYSTEM_ARTIFACTS_DIR}"
+                    displayStackDir "${SYSTEM_ARTIFACTS_DIR}/${STACK}/GitRepo/${STACK_VERSION}"                    
 
-                if [ "$BUILD_REASON" != "PullRequest" ]; then
-                    docker push $BuildVerRepoTag
-                    docker tag $BuildVerRepoTag $MCRRepoTag
-                    docker push $MCRRepoTag
-                fi
+                    cd "${SYSTEM_ARTIFACTS_DIR}/${STACK}/GitRepo/${STACK_VERSION}"                    
+                    displayInformationRegardingImageToBeBuilt $BuildVerRepoTag $appSvcDockerfilePath
 
-                echo $MCRRepoTag >> $SYSTEM_ARTIFACTS_DIR/${STACK}builtImageList
-            done
+                    # php-xdebug depends of published images
+                    if [ "$BUILD_REASON" != "PullRequest" ] || [ "$STACK" != "php-xdebug" ]; then
+                        echo docker build -t "$BuildVerRepoTag" -f "$appSvcDockerfilePath" .
+                        docker build -t "$BuildVerRepoTag" -f "$appSvcDockerfilePath" .
+                    elif [ "$BUILD_REASON" != "PullRequest" ] && [ "$STACK" == "php-xdebug" ]; then
+                        # poll until php image is published
+                        BASE_TAG=`head -n 1 Dockerfile | sed 's/FROM //g'`
+                        PULL_OUTPUT=`docker pull $BASE_TAG`
+                        LOOP_COUNTER=0;
+                        while [[ $PULL_OUTPUT != *"up to date"* ]] && [ $LOOP_COUNTER -le 60 ]; do
+                            sleep 1m
+                            PULL_OUTPUT=$(docker pull $BASE_TAG)
+                            LOOP_COUNTER=$LOOP_COUNTER+1
+                        done;
+                        echo docker build -t "$BuildVerRepoTag" -f "$appSvcDockerfilePath" .
+                        docker build -t "$BuildVerRepoTag" -f "$appSvcDockerfilePath" .
+                    fi
+
+                    if [ "$BUILD_REASON" != "PullRequest" ]; then
+                        docker push $BuildVerRepoTag
+                        docker tag $BuildVerRepoTag $MCRRepoTag
+                        docker push $MCRRepoTag
+                    fi
+
+                    echo $MCRRepoTag >> $SYSTEM_ARTIFACTS_DIR/${STACK}builtImageList
+                done
+            fi
+            
         done < "$CONFIG_DIR/${STACK}VersionTemplateMap.txt"
     elif [[ $STACK = "DiagnosticServer" ]]; then
         # DiagnosticServer Image
@@ -96,12 +131,10 @@ function buildDockerImage()
         local MCRRepoTag="${MCRRepoTagUpperCase,,}"
         local appSvcDockerfilePath="${SYSTEM_ARTIFACTS_DIR}/${STACK}/${FILES_ROOT_PATH}/Dockerfile"
 
-        echo "Listing artifacts dir"
-        ls "${SYSTEM_ARTIFACTS_DIR}"
-        echo "Listing stacks dir"
+        displayArtifactsDir "${SYSTEM_ARTIFACTS_DIR}"
         cd "${SYSTEM_ARTIFACTS_DIR}/${STACK}/${FILES_ROOT_PATH}"
-        echo
-        echo "Building test image with tag '$BuildVerRepoTag' and file $appSvcDockerfilePath..."
+
+        displayInformationRegardingImageToBeBuilt $BuildVerRepoTag $appSvcDockerfilePath
         echo docker build -t "$BuildVerRepoTag" -f "$appSvcDockerfilePath" .
         docker build -t "$BuildVerRepoTag" -f "$appSvcDockerfilePath" .
         docker tag $BuildVerRepoTag $MCRRepoTag
@@ -123,12 +156,9 @@ function buildDockerImage()
         local MCRRepoTag="${MCRRepoTagUpperCase,,}"
         local appSvcDockerfilePath="${SYSTEM_ARTIFACTS_DIR}/${STACK}/${FILES_ROOT_PATH}/Dockerfile"
 	
-        echo "Listing artifacts dir"
-        ls "${SYSTEM_ARTIFACTS_DIR}"
-        echo "Listing stacks dir"
+        displayArtifactsDir "${SYSTEM_ARTIFACTS_DIR}"
         cd "${SYSTEM_ARTIFACTS_DIR}/${STACK}/${FILES_ROOT_PATH}"
-        echo
-        echo "Building test image with tag '$BuildVerRepoTag' and file $appSvcDockerfilePath..."
+        displayInformationRegardingImageBeingBuilt $BuildVerRepoTag $appSvcDockerfilePath
         echo docker build -t "$BuildVerRepoTag" -f "$appSvcDockerfilePath" .
         docker build -t "$BuildVerRepoTag" -f "$appSvcDockerfilePath" .
         docker tag $BuildVerRepoTag $MCRRepoTag
