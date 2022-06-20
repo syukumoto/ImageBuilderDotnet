e3b0f75cba93a718a8b07a0a85793acfd5eba858
saisubodh_MSFT <saisubodh@microsoft.com>
2022-06-21 03:12:27 +0530

    WordPress bug fix: CDN urls not updating due to redis cache

--- GenerateDockerFiles/wordpress/wordpress_with_mariadb/local_bin/w3tc_cdn_config.sh
+++ GenerateDockerFiles/wordpress/wordpress_with_mariadb/local_bin/w3tc_cdn_config.sh
@@ -10,6 +10,9 @@ if [[ $response == "200" ]]; then
             echo "CDN_CONFIGURATION_COMPLETE" >> $WORDPRESS_LOCK_FILE
             #stop atd daemon
             service atd stop
+        
+            #purge redis cache
+            redis-cli flushall
         else
     	    service atd start
             echo 'bash /usr/local/bin/w3tc_cdn_config.sh' | at now +5 minutes
