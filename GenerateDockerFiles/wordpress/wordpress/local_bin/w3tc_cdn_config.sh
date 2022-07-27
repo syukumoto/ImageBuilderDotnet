198f16c86938a15d1d9fc6f3cb5a813196a31f25
saisubodhMSFT <saisubodh@microsoft.com>
2022-07-28 02:53:38 +0530

    Wordpress enable CDN + Blob Storage configuration

--- GenerateDockerFiles/wordpress/wordpress/local_bin/w3tc_cdn_config.sh
+++ GenerateDockerFiles/wordpress/wordpress/local_bin/w3tc_cdn_config.sh
@@ -4,21 +4,24 @@ response=$(curl --write-out '%{http_code}' --silent --output /dev/null {https://
 if [[ $response == "200" ]]; then
     #Configure CDN settings 
     if [[ $CDN_ENDPOINT ]]; then
-        if wp w3-total-cache option set cdn.enabled true --type=boolean --path=$WORDPRESS_HOME --allow-root \
+        if [[ "$0" == "BLOB_CDN" ]] && [ ! $(grep "BLOB_CDN_CONFIGURATION_COMPLETE" $WORDPRESS_LOCK_FILE) ] \
+        && wp w3-total-cache option set cdn.mirror.domain $CDN_ENDPOINT --type=array --path=$WORDPRESS_HOME --allow-root; then
+            echo "BLOB_CDN_CONFIGURATION_COMPLETE" >> $WORDPRESS_LOCK_FILE
+            service atd stop
+            redis-cli flushall
+        else if [ ! $(grep "CDN_CONFIGURATION_COMPLETE" $WORDPRESS_LOCK_FILE) ] \
+        && wp w3-total-cache option set cdn.enabled true --type=boolean --path=$WORDPRESS_HOME --allow-root \
         && wp w3-total-cache option set cdn.engine "mirror" --path=$WORDPRESS_HOME --allow-root \
         && wp w3-total-cache option set cdn.mirror.domain $CDN_ENDPOINT --type=array --path=$WORDPRESS_HOME --allow-root; then
             echo "CDN_CONFIGURATION_COMPLETE" >> $WORDPRESS_LOCK_FILE
-            #stop atd daemon
             service atd stop
-            
-            #purge redis cache
             redis-cli flushall
         else
     	    service atd start
-            echo 'bash /usr/local/bin/w3tc_cdn_config.sh' | at now +5 minutes
+            echo 'bash /usr/local/bin/w3tc_cdn_config.sh $0' | at now +5 minutes
         fi
     fi
 else
     service atd start
-    echo 'bash /usr/local/bin/w3tc_cdn_config.sh' | at now +5 minutes
+    echo 'bash /usr/local/bin/w3tc_cdn_config.sh $0' | at now +5 minutes
 fi
