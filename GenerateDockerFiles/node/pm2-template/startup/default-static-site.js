f23f4f8ec644797b75e30b7f8488f6c69187084a
Eric Stenson <ericsten@php.net>
2022-07-26 21:24:09 -0700

    TuxProxy timeout is 60 seconds.  node.js default is 5 seconds.

--- GenerateDockerFiles/node/pm2-template/startup/default-static-site.js
+++ GenerateDockerFiles/node/pm2-template/startup/default-static-site.js
@@ -3,5 +3,9 @@ var server = express();
 var options = {
     index: ['index.html','hostingstart.html']
 };
+// Must be longer than local proxy keep-alive timeout
+server.keepAliveTimeout = (65 * 1000);
+// Must be longer than server.keepAliveTimeout
+server.headersTimeout = (66 * 1000);
 server.use('/', express.static('/opt/startup', options));
 server.listen(process.env.PORT);
