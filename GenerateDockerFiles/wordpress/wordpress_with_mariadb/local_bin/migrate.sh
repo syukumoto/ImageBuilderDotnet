32c36a27e83b2d600bf346f2e3e12d238abbe203
saisubodhMSFT <saisubodh@microsoft.com>
2022-11-09 11:30:11 +0530

    remove debug messages

--- GenerateDockerFiles/wordpress/wordpress_with_mariadb/local_bin/migrate.sh
+++ GenerateDockerFiles/wordpress/wordpress_with_mariadb/local_bin/migrate.sh
@@ -7,7 +7,6 @@ if (( $trycount > 0 )); then
 	if [[ $MIGRATION_IN_PROGRESS ]] && [[ "$MIGRATION_IN_PROGRESS" == "true" || "$MIGRATION_IN_PROGRESS" == "TRUE" || "$MIGRATION_IN_PROGRESS" == "True" ]]  \
 	&& [[ $MIGRATE_NEW_DATABASE_NAME ]] && [[ $MIGRATE_MYSQL_DUMP_PATH ]] \
 	&& [ ! $(grep "MYSQL_DB_IMPORT_COMPLETED" $mysql_import_statusfile_path) ]; then
-		echo "importing_mysql" >> $WORDPRESS_LOCK_FILE
 		if apk add mysql-client --no-cache \
 		&& mysql -h $DATABASE_HOST -u $DATABASE_USERNAME --password=$DATABASE_PASSWORD -e "DROP DATABASE IF EXISTS $MIGRATE_NEW_DATABASE_NAME; CREATE DATABASE $MIGRATE_NEW_DATABASE_NAME;" --ssl=true \
 		&& mysql -h $DATABASE_HOST -u $DATABASE_USERNAME --password=$DATABASE_PASSWORD $MIGRATE_NEW_DATABASE_NAME < $MIGRATE_MYSQL_DUMP_PATH  --ssl=true; then
